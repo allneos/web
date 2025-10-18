7eb65c3c9052a8979ec4a3e2741ebf1b80a1042f
jmosias <jmosias@outlook.com>
2025-10-18 10:11:25 +0800

    build: init

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+
+  return (
+    <>
+      <header className="p-4 flex items-center">
+        <h1 className="ml-4 text-xl font-semibold">
+          Allneos
+        </h1>
+      </header>
+    </>
+  )
+}
